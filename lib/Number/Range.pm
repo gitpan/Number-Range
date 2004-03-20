a609ddc8ee87fa60ec0d82b71a702faed1256d38
Larry Shatzer, Jr. <larrysh@cpan.org>
2004-03-20 21:21:05 +0000

    Import of LARRYSH/Number-Range-0.02 from CPAN.
    gitpan-cpan-distribution: Number-Range
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         LARRYSH/Number-Range-0.02.tar.gz
    gitpan-cpan-author:       LARRYSH
    gitpan-cpan-maturity:     released

--- lib/Number/Range.pm
+++ lib/Number/Range.pm
@@ -10,90 +10,135 @@ require Exporter;
 our @ISA = qw(Exporter);
 
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 sub new {
-	my $this = shift;
-	my $class = ref($this) || $this;
-	my $self = {};
-	bless $self, $class;
-	$self->initialize(@_);
-	return $self;
+  my $this = shift;
+  my $class = ref($this) || $this;
+  my $self = {};
+  bless $self, $class;
+  $self->initialize("add", @_);
+  return $self;
 }
 
 sub initialize {
   my $self = shift;
+  my $type = shift;
   my $rangesep = qr/(?:\.\.)/;
   my $sectsep  = qr/(?:\s|,)/;
   my $validation = qr/(?:
-       [^0-9,. -]|        # These are the only allowed characters (Numbers and "seperators")
-       $rangesep$sectsep| # We don't want a range seperator followed by section seperator
-       $sectsep$rangesep| # We don't want a section seperator followed by range seperator
+       [^0-9,. -]|        # These are the only allowed characters (Numbers and "separators")
+       $rangesep$sectsep| # We don't want a range separator followed by section separator
+       $sectsep$rangesep| # We don't want a section separator followed by range separator
        \d-\d|             # We don't want 10-10 since - is for negative numbers
-       ^$sectsep|         # We don't want a section seperator at the start
-       ^$rangesep|        # We don't want a range seperator at the start
-       $sectsep$|         # We don't want a section seperator at the end
-       $rangesep$         # We don't want a range seperator at the end
+       ^$sectsep|         # We don't want a section separator at the start
+       ^$rangesep|        # We don't want a range separator at the start
+       $sectsep$|         # We don't want a section separator at the end
+       $rangesep$         # We don't want a range separator at the end
        )/x;
   foreach my $item (@_) {
- 	  croak "$item contains invalid data" if ($item =~ m/$validation/g);
- 	  foreach my $section (split(/$sectsep/, $item)) {
- 	  	if ($section =~ m/$rangesep/) {
- 	  		my ($start, $end) = split(/$rangesep/, $section, 2);
- 	  		if ($start > $end) {
- 	  			carp "$start is > $end" if (warnings::enabled());
- 	  			($start, $end) = ($end, $start);
- 	  		}
- 	  		if ($start == $end) {
- 	  			carp "$start:$end is pointless" if (warnings::enabled());
- 	  			$self->_addnumbers($start);
- 	  		}
- 	  		else {
- 	  		  $self->_addnumbers($start .. $end);
- 	  		}
- 	  	}
- 	  	else {
- 	  		$self->_addnumbers($section);
- 	  	}
- 	  }
+    croak "$item contains invalid data" if ($item =~ m/$validation/g);
+    foreach my $section (split(/$sectsep/, $item)) {
+      if ($section =~ m/$rangesep/) {
+        my ($start, $end) = split(/$rangesep/, $section, 2);
+        if ($start > $end) {
+          carp "$start is > $end" if (warnings::enabled());
+          ($start, $end) = ($end, $start);
+        }
+        if ($start == $end) {
+          carp "$start:$end is pointless" if (warnings::enabled());
+          if ($type eq "add") {
+            $self->_addnumbers($start);
+          }
+          elsif ($type eq "del") {
+            $self->_delnumbers($start);
+          }
+          else {
+            die "Neither 'add' nor 'del' was passed initialize()";
+          }
+        }
+        else {
+          if ($type eq "add") {
+            $self->_addnumbers($start .. $end);
+          }
+          elsif ($type eq "del") {
+            $self->_delnumbers($start .. $end);
+          }
+          else {
+            die "Neither 'add' nor 'del' was passed initialize()";
+          }
+        }
+      }
+      else {
+        if ($type eq "add") {
+           $self->_addnumbers($section);
+         }
+         elsif ($type eq "del") {
+          $self->_delnumbers($section);
+         }
+         else {
+          die "Neither 'add' nor 'del' was passed initialize()";
+        }
+      }
+    }
   }
 }
 
 sub _addnumbers {
-	my $self = shift;
-	foreach my $number (@_) {
-		if (warnings::enabled()) {
-	    carp "$number already in range" if $self->inrange($number);
-	  }
-		$self->{_rangehash}{$number} = 1;
-	}
+  my $self = shift;
+  foreach my $number (@_) {
+    if (warnings::enabled()) {
+      carp "$number already in range" if $self->inrange($number);
+    }
+    $self->{_rangehash}{$number} = 1;
+  }
+}
+
+sub _delnumbers {
+  my $self = shift;
+  foreach my $number (@_) {
+    if (warnings::enabled()) {
+      carp "$number not in range or already removed" if (!$self->inrange($number));
+    }
+    delete $self->{_rangehash}{$number};
+  }
 }
 
 sub inrange {
-	my $self   = shift;
-	my $number = shift;
+  my $self   = shift;
+  my $number = shift;
   return (exists($self->{_rangehash}{$number})) ? 1 : 0;
 }
 
+sub addrange {
+  my $self = shift;
+  $self->initialize("add", @_);
+}
 
+sub delrange {
+  my $self = shift;
+  $self->initialize("del", @_);
+}
 1;
 __END__
 
 =head1 NAME
 
-Number::Range - Perl extension defining ranges of numbers and testing 
+Number::Range - Perl extension defining ranges of numbers and testing
 later if a number is in it
 
 =head1 SYNOPSIS
 
   use Number::Range;
-  
+
   my $range = Number::Range->new("-10..10,12,100..120");
   if ($range->inrange("13")) {
-  	print "In range\n";
+    print "In range\n";
   } else {
-  	print "Not in range\n";
+    print "Not in range\n";
   }
+  $range->addrange("200..300");
+  $range->delrange("250..255");
 
 =head1 DESCRIPTION
 
@@ -105,6 +150,24 @@ test later on if a number falls within the range(s).
 The format used for range is pretty straight forward. To separate sections
 of ranges it uses a C<,> or whitespace. To create the range, it uses C<..> to
 do this, much like Perl's own binary C<..> range operator in list context.
+
+=head2 METHODS
+
+=item $range = Number::Range->new("10..20","25..30");
+
+Creates the range object. It will accept any number of ranges as its input.
+
+=item $range->addrange("22");
+
+This will also take any number of ranges as input and add them to the existing range.
+
+=item $range->delrange("10");
+
+This will also take any number of ranges as input and delete them from the existing range.
+
+=item $range->inrange("26");
+
+This will take one number and check if it exists in the range and return C<1> for true and C<0> for false.
 
 =head2 EXPORT
 
--- t/Number-Range.t
+++ t/Number-Range.t
@@ -1,19 +1,26 @@
-use Test::More tests => 16;
+use Test::More tests => 19;
 BEGIN { use_ok('Number::Range') };
-
 
 ok($range = Number::Range->new("10..100"));
 ok($range->inrange(10)   == 1);
 ok($range->inrange(1000) == 0);
-ok($range = Number::Range->new("10..50,60..100"));
+$range = Number::Range->new("10..50,60..100");
 ok($range->inrange(10) == 1);
 ok($range->inrange(55) == 0);
 ok($range->inrange(75) == 1);
-ok($range = Number::Range->new("10..100","150..200"));
+$range = Number::Range->new("10..100","150..200");
 ok($range->inrange(10)  == 1);
 ok($range->inrange(125) == 0);
 ok($range->inrange(155) == 1);
-ok($range = Number::Range->new("-10..10"));
+$range = Number::Range->new("-10..10");
 ok($range->inrange(10)  == 1);
 ok($range->inrange(-10) == 1);
 ok($range->inrange(0)   == 1);
+$range->addrange("20..30");
+ok($range->inrange(25) == 1);
+ok($range->inrange(10) == 1);
+ok($range->inrange(15) == 0);
+$range->delrange("-10..0");
+ok($range->inrange(-10) == 0);
+ok($range->inrange(10)  == 1);
+ok($range->inrange(25) == 1);
